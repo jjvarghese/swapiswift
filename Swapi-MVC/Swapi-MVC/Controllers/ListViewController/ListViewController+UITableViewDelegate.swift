9cb0c4f0b72cd6bf276b422f801b3b11ce99bc8f
Joshua J Varghese <joshua.varghese@curalie.com>
2022-12-25 13:18:49 +0100

    add DetailViewController

--- Swapi-MVC/Swapi-MVC/Controllers/ListViewController/ListViewController+UITableViewDelegate.swift
+++ Swapi-MVC/Swapi-MVC/Controllers/ListViewController/ListViewController+UITableViewDelegate.swift
@@ -10,4 +10,22 @@ import UIKit
 
 extension ListViewController: UITableViewDelegate {
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let person = people[indexPath.row]
+
+        let title = person.name
+        let detailOne = "Gender: " + person.gender
+        let detailTwo = "Hair colour: " + person.hairColor
+        let detailThree = "Eye colour: " + person.eyeColor
+        let detailFour = "Skin colour: " + person.skinColor
+
+        let detailVC = DetailViewController(withTitle: title,
+                                            detailOne: detailOne,
+                                            detailTwo: detailTwo,
+                                            detailThree: detailThree,
+                                            detailFour: detailFour)
+
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+
 }
